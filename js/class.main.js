e184a3ad2267563c799a714f4fe4c32eef87bde3
DAMIE, Nathan <ndamie@LFR409570.corp.capgemini.com>
2014-01-15 11:28:36 +0100

    improve sample

--- js/class.main.js
+++ js/class.main.js
@@ -32,8 +32,9 @@ var CGMain = CGSGView.extend(
             this.accordion = new CGSGAccordion(15, 15, 400, 800, 50);
 
             //Build the section.
-            this.titleSection = this._buildTitleSection();
-            this.previewSection = this._buildPreviewSection();
+            this._buildTitleSection();
+            this._buildPreviewSection();
+            this._buildSectionWithComplexCore();
 
             this.rootNode.addChild(this.accordion);
 
@@ -50,6 +51,7 @@ var CGMain = CGSGView.extend(
             core.color = "blue";
             core.addChild(new CGSGNodeText(20, 10, "I'm the core !"));
 
+            //Create the new section and add it to the accordion
             this.accordion.buildAndAddSection("Simple section", null, core);
 
         },
@@ -66,6 +68,56 @@ var CGMain = CGSGView.extend(
             core.addChild(new CGSGNodeText(20, 10, "I'm the core !"));
 
             this.accordion.buildAndAddSection("Preview Section", preview, core);
+
+        },
+
+        _buildSectionWithComplexCore: function () {
+            var tooltipNode = new CGSGNode(5, 0),
+                width = 400 - 10,
+                iconPadding = 80,
+                textNode = new CGSGNodeSquare(0, 10, width, 115),
+                textNodeLabel = new CGSGNodeText(iconPadding + 20, 10, "You can also :\n   -change section's style\n   -use event\n  -..."),
+                circle = new CGSGNodeCircle(50, 55, 30);
+
+            textNodeLabel.setWrapMode(CGSGWrapMode.WORD);
+
+            var padding = 5;
+            textNodeLabel.translateWith(padding, padding);
+            textNodeLabel.setTypo('Arial', true);
+            textNodeLabel.setSize(15, true);
+            textNodeLabel.setMaxWidth((width - iconPadding) - (padding << 1), true);
+            textNodeLabel.setLineHeight(20, true);
+
+            textNode.color = "rgba(170,170,170,0.5)";
+            textNode.addChild(textNodeLabel);
+
+            circle.color = 'white';
+
+            this.titleNode = new CGSGNodeButton(0, 130, "Select other section");
+            var dimension = new CGSGDimension(width, 50);
+            this.titleNode.setFixedSize(dimension);
+            this.titleNode.resizeTo(dimension.width, dimension.height);
+
+            this.titleNode.onClick = function () {
+                this.accordion.selectSection(this.accordion.sections[1]);
+
+            }.bind(this);
+
+            textNode.addChild(circle);
+
+            tooltipNode.addChild(textNode);
+            tooltipNode.addChild(this.titleNode);
+
+            tooltipNode.resizeWith(textNode.getWidth(), textNode.getHeight() + this.titleNode.getHeight() + 20);
+
+            var core = new CGSGNode(0,0);
+            core.resizeTo(this.accordion.getWidth(), 200);
+            core.addChild(tooltipNode);
+
+            var section = this.accordion.buildAndAddSection("Section with complex core", null, core);
+            section._backgroundColor = "black";
+            section.title.color = "#605C55";
+            section._puceColor = "#605C55";
         }
     }
 );
